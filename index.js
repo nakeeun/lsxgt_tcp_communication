4cb426723933a204bbc8997d7687ffb35f61d777
Nakeun Choi <37968684+nakeeun@users.noreply.github.com>
2021-10-31 19:20:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const net = require('net');
 const XGTsocket = net.createConnection({port: 2004, host: '192.168.0.120' });
-
 XGTsocket.on('connect', () => {
   var dataAddr = '%DW100';
   var temp = writeData(dataAddr);
@@ -14,6 +13,7 @@ XGTsocket.on('data', serverData => {
   console.log(Buffer.from(serverData));
   XGTsocket.destroy();
 });
+
 var companyHeader = function(dataFrame){
   var company_id = Buffer.from('LSIS-XGT');
   var reserved1 = Buffer.from([0x00,0x00]);
@@ -32,21 +32,62 @@ var companyHeader = function(dataFrame){
   console.log(data);                               
   return data;                               
 }
-var readData = function(address){
+var readData = function(address,dataType){
   var data;
-  var command = Buffer.from([0x54,0x00]);              //read(0x54), write(0x58)
-  var dataType = Buffer.from([0x02,0x00]);
+  var command = Buffer.from([0x54,0x00]);
+  var set_data_Type = setDataType(dataType);
   var reserved = Buffer.from([0x00,0x00]);
   var block = Buffer.from([0x01,0x00]);
-  var variable = Buffer.from(address, 'utf8');
-  var var_length = Buffer.from([variable.length,0x00]);
-  var total_length = command.length+dataType.length+reserved.length+block.length+var_length.length+variable.length;
-  data = Buffer.concat([command, dataType,reserved,block,var_length,variable],total_length);
+  var addr = Buffer.from(address, 'utf8');
+  var addr_length = Buffer.from([addr.length,0x00]);
+  var total_length = command.length+set_data_Type.length+reserved.length+block.length+addr_length.length+addr.length;
+  data = Buffer.concat([command, set_data_Type,reserved,block,addr_length,addr],total_length);
   console.log(data);
   return data;
 }
-var request_data = function(reqData,dataType){
+var writeData = function(address,dataType,tempData){
+    var data;
+    var command = Buffer.from([0x58,0x00]);
+    var set_data_Type = setDataType(dataType);
+    var reserved = Buffer.from([0x00,0x00]);
+    var block = Buffer.from([0x01,0x00]);
+    var addr = Buffer.from(address, 'utf8');
+    var addr_length = Buffer.from([addr.length,0x00]);
+    var data = Buffer.from(tempData, 'utf-8');
+    var data_length = Buffer.from([data.length,0x00]);
+    var total_length = command.length+set_data_Type.length+reserved.length+block.length+addr_length.length+addr.length+data.length+data_length.length;
+    data = Buffer.concat([command, set_data_Type,reserved,block,addr_length,addr,data_length,data],total_length);
+    console.log(data);
+    return data;
+  }
+
+  var request_data = function(reqData){
     XGTsocket.write(reqData);
+  }
+
+var setDataType = function(dataType){
+  var temp;
+  switch(dataType){
+    case 'bit':
+      temp = Buffer.from([0x00,0x00]);
+      break;
+    case 'byte':
+      temp = Buffer.from([0x01,0x00]);
+      break;
+    case 'word':
+      temp = Buffer.from([0x02,0x00]);
+      break;
+    case 'dword':
+      temp = Buffer.from([0x03,0x00]);
+      break;
+    case 'lword':
+      temp = Buffer.from([0x04,0x00]);
+      break;
+    case 'seq':
+      temp = Buffer.from([0x14,0x00]);
+      break;
+  }
+  return temp;
 }
 /*setInterval(() => {
 
